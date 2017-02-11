2ceb577e3a319a3f4dbbd4db218b05c1ea6cc332
Micheal Liu <michael.liu0@gmail.com>
2017-02-11 13:22:30 -0800

    Initial Commit

---
+++ Assets/Game.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+	
+	[SerializeField]
+	private Transform player, camera;
+	
+	[SerializeField]
+	private Transform CamPositionTop, CamPositionSide, CamPositionIso;
+
+    private IEnumerator camMoveCoroutine = null;
+	
+	private enum Mode{
+		Top, Side, Iso
+	}
+	private Mode mode;
+
+	// Use this for initialization
+	void Start () {
+		mode = Mode.Top;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (Input.GetKeyDown(KeyCode.D)) {
+            camMoveCoroutine = moveCamera(Mode.Side);
+        } else if( Input.GetKeyDown ( KeyCode.W ) ) {
+            camMoveCoroutine = moveCamera ( Mode.Top );
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            camMoveCoroutine = moveCamera ( Mode.Iso );
+        }
+		
+		Vector3 move = Vector3.zero;
+		
+		if( mode == Mode.Top ){
+			
+			if( Input.GetKey( KeyCode.RightArrow ) ){
+		        move += transform.right * 0.1f;
+		    }
+			
+		}
+
+	    player.transform.position += move;
+
+	}
+
+    IEnumerator moveCamera(Mode targetMode) {
+
+        //Indicator for whether we're done
+        bool finished = false;
+
+        Transform target;
+        switch (targetMode) {
+            case Mode.Top:
+                target = CamPositionTop;
+                break;
+            case Mode.Side:
+                target = CamPositionSide;
+                break;
+            case Mode.Iso:
+                target = CamPositionIso;
+                break;
+            default:
+                yield break;
+        }
+
+        do {
+            camera.position = Vector3.Lerp ( camera.position, target.position, 0.2f );
+            camera.rotation = Quaternion.Slerp(camera.rotation, target.rotation, 0.2f);
+            if (Vector3.Distance(target.position, camera.position) < 0.001f) {
+                finished = true;
+            }
+
+            yield return 1; // Break for a frame
+
+        } while (!finished);
+
+        print("Ho!");
+
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private CharacterController charCon;
+    private Vector3 moveVector;
+    private Quaternion lookVector;
+    private float rotX, rotY;
+
+	// Use this for initialization
+	void Start () {
+		charCon = gameObject.GetComponent<CharacterController>();
+        lookVector = Quaternion.identity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = Vector3.zero;
+	    if (Input.GetKey(KeyCode.W)) {
+	        moveVector += Vector3.forward;
+	    }
+
+        charCon.SimpleMove(moveVector);
+
+	    rotX += Input.GetAxis ( "Mouse X" );
+	    rotY += Input.GetAxis("Mouse Y");
+
+        Camera.main.transform.localEulerAngles = new Vector3 ( -rotY, rotX, 0 );
+
+        //lookVector *= Quaternion.AngleAxis(Input.GetAxis("Mouse X"), transform.up) * Quaternion.AngleAxis( Input.GetAxis("Mouse Y"), transform.right);
+	    //Camera.main.transform.rotation = lookVector;
+	}
+}
