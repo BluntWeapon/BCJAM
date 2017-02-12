f26d73e7b60d1660e2ca9b9fe4e370582a50370c
Micheal Liu <michael.liu0@gmail.com>
2017-02-11 21:59:50 -0800

    Jumping in Side View

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -34,16 +34,29 @@ public class Game : MonoBehaviour {
 
     private bool isSwappingPerspective = false;
 
+    [SerializeField] private float jumpVelocity = 4f, gravity = 3f;
+    private float jumping = 0f;
+
 	// Use this for initialization
 	void Start () {
 		mode = Mode.Top;
 	    controller = player.GetComponent<CharacterController>();
 	    playerRenderer = player.gameObject.GetComponent<Renderer>();
-	}
+
+        //Disable all blue colliders
+        foreach( Collider collider in blueColliders ) {
+            collider.enabled = false;
+        }
+        //Enable all red colliders
+        foreach( Collider collider in redColliders ) {
+            collider.enabled = true;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
+        //TODO This doesn't check if dimensions can be changed
         //Camera Movement on Perspective Change
 	    IEnumerator newCamCoroutine = null;
 	    if (Input.GetKeyDown(KeyCode.D)) {
@@ -124,56 +137,26 @@ public class Game : MonoBehaviour {
                 move += transform.right * -0.1f;
             }
 
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                jumping = jumpVelocity;
+            }
+            else if ( !controller.isGrounded ) {
+                jumping -= gravity;
+            } else if (controller.isGrounded) {
+                jumping = 0f;
+            }
+
+            move += transform.up * jumping;
+
         }
 
         controller.Move(move);
-
-        //Raycast downwards from edge. Detects green, red and blue box collisions and passes.
-        raycastDetection();
-
-    }
-
-    //Indicator for whether we're done
-    void raycastDetection() {
-        // -A explanation-
-        // This creates a copy of the Vector3 instead of referencing it from GameObject.Find actual Object. V3's are struct. Structs are on the stack instead of the heap.
-        // This is different from how C++ deals with Structs, because this is C#. Structs vs Object. C# has this integrated into because it has C++ blood.
-        float playerY = 0; // If statements won't initialize variables. This needs to be here to initialize.
-        Vector3 playerPosition = GameObject.Find("Player").transform.position;  
-        Vector3 hoverDistance = new Vector3 (0, 10.0f, 0);
-        Vector3 rayOrigin = new Vector3(playerPosition.x, playerPosition.y - (playerSize.y / 2), playerPosition.z);
-        Vector3 btm = GameObject.Find("Player").transform.TransformDirection(-Vector3.up);
-        // Vector3 fwd = GameObject.Find("Player").transform.TransformDirection(Vector3.right);
-
-        Ray ray = new Ray (rayOrigin, btm);
-        RaycastHit hit;
-
-        if (mode == Mode.Top)
-        {
-            GameObject.Find("Player").transform.position = new Vector3(playerPosition.x, 5, playerPosition.z); // Add fixed distance from topmost surface.
-            // playerPosition = new Vector3 (playerPosition.x, 5, playerPosition.z); // This is incorrectly. // Refer to -A explanation- 
-        }
-
-        if (Physics.Raycast(ray, out hit)) {
-            Debug.DrawRay( ray.origin, hit.point - ray.origin, Color.red);
-            // Debug.Log(hit.point.y);
-            playerY = hit.point.y;
-        }
-
-        if (mode == Mode.Side) {
-            GameObject.Find("Player").transform.position = new Vector3(playerPosition.x, playerY + (playerSize.y / 2), playerPosition.z); // Add fixed distance from topmost surface.
-            // playerPosition = new Vector3 (playerPosition.x, 5, playerPosition.z); // This is incorrectly. // Refer to -A explanation- 
-        }
-
 
     }
 
     IEnumerator moveCamera(Mode targetMode) {
 
-        if( mode == Mode.Top ) {//TODO This check should be done somewhere else
-            //TODO Drop player
-            Debug.Log ( "Ho!" );
-
+        if( mode == Mode.Top && targetMode != Mode.Top ) {//TODO This check should be done somewhere else
             RaycastHit hit;
             Physics.Raycast ( new Ray ( player.position, Vector3.down ), out hit, 60f );
             player.position = new Vector3 ( player.position.x, hit.point.y + 1.001f, player.position.z );
